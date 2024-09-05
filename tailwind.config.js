f2750d7825c8c708f7e181533e3dce091b411d2b
dhrumilpatel30 <dhrumilp063@gmail.com>
2024-09-05 21:41:45 +0530

    add: next version

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,19 +4,21 @@ export default {
     content: ["./index.html"],
     theme: {
         fontFamily: {
-            'mono': ["'Geist Mono'", 'ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'Consolas', "'Liberation Mono'", "'Courier New'", 'monospace'],
+            'sans': ["'Inter'", 'ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'Consolas', "'Liberation Mono'", "'Courier New'", 'monospace'],
         },
         colors: {
             transparent: 'transparent',
             current: 'currentColor',
             'background-dark': '#100e05',
-            'foreground-dark': '#ceccc5',
-            'background-secondary-dark': 'rgba(31,31,28,0.34)',
+            // 'foreground-dark': '#ceccc5',
+            'foreground-dark': '#ccc8c1',
+            // 'background-secondary-dark': 'rgba(31,31,28,0.34)',
+            'background-secondary-dark': '#15130b',
             'foreground-secondary-dark': '#DEDE8F',
             'background-light': '#d6d2c1',
             'foreground-light': '#0F0F0D',
             'background-secondary-light': 'rgba(204,200,184,0.65)',
-            'foreground-secondary-light': '#6d6f3b',
+            'foreground-secondary-light': '#5b4805',
         },
     },
     extend: {
