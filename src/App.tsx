87e85d0254df2ddeb3036dede32879ba67151817
dhrumilpatel30 <dhrumilp063@gmail.com>
2025-04-28 19:03:22 +0530

    Remove: Google tag mangaer

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import "./App.css";
 import About from "./components/About";
 import Contact from "./components/Contact";
@@ -15,25 +14,6 @@ declare global {
 }
 
 const App = () => {
-  useEffect(() => {
-    // Add Google Analytics
-    const script = document.createElement("script");
-    script.async = true;
-    script.src = "https://www.googletagmanager.com/gtag/js?id=G-98VLT31M6C";
-    document.head.appendChild(script);
-
-    window.dataLayer = window.dataLayer || [];
-    function gtag(...args: any[]) {
-      window.dataLayer.push(args);
-    }
-    gtag("js", new Date());
-    gtag("config", "G-98VLT31M6C");
-
-    return () => {
-      document.head.removeChild(script);
-    };
-  }, []);
-
   return (
     <div className="shiny h-full dark:bg-[#100e05] dark:text-[#ccc8c1] bg-red">
       <div className="body-full">
